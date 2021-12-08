50dcba61f03f8e2bfd1f7c1f7330017c42019401
dogodo-cc <741484865@qq.com>
2021-12-08 14:34:53 +0800

    feat: 增加ids 列表

--- projects/github-ids/scripts/content.js
+++ projects/github-ids/scripts/content.js
@@ -1,12 +1,39 @@
 console.log('我是 @cocos-fe/githubs-ids 小插件，我被启用了！');
 
-function createIcon() {
+const $panel = document.createElement('div');
+$panel.id = 'githubs-ids';
+
+function createPanel() {
   const image = chrome.runtime.getURL('images/logo.png');
-  const $icon = document.createElement('div');
-  $icon.id = 'githubs-ids';
-  $icon.innerHTML = `<img src="${image}" alt="githubs-ids" />`;
-  $icon.addEventListener('click', replaceIds, false);
-  document.body.appendChild($icon);
+
+  $panel.innerHTML = `
+    <div class="icon">
+      <img src="${image}" alt="githubs-ids" />
+    </div>
+    <ul></ul>
+  `;
+
+  $panel.querySelector('.icon').addEventListener('click', () => {
+    $panel.classList.toggle('show');
+    if ($panel.querySelectorAll('ul li').length === 0) {
+      replaceIds();
+    }
+  }, false);
+
+  $panel.querySelector('ul').addEventListener('click', ({ target }) => {
+    copy(target.innerText);
+  })
+  document.body.appendChild($panel);
+}
+
+function createList(data) {
+  const fragment = document.createDocumentFragment();
+  Object.entries(data).forEach(([k, v]) => {
+    const li = document.createElement('li');
+    li.innerHTML = `<div class="en">${k}</div><div class="zh">${v}</div>`;
+    fragment.appendChild(li);
+  });
+  $panel.querySelector('ul').appendChild(fragment);
 }
 
 // 替换id
@@ -14,7 +41,7 @@ function replaceIds() {
   const url = `https://90s.oss-cn-hangzhou.aliyuncs.com/github-ids/github-ids.json?v=${Date.now()}`;
 
   fetch(url).then(res => res.json()).then(data => {
-    console.log(data)
+    createList(data);
 
     Array.from(document.querySelectorAll('.author, .assignee span, .TimelineItem-body a span'))
       .forEach(ele => {
@@ -36,7 +63,7 @@ function replaceIds() {
     replaceIds();
     if (e === 'load') {
       observerProgress();
-      createIcon();
+      createPanel();
     }
   })
 })
@@ -61,4 +88,64 @@ function observerProgress() {
 }
 
 
+
+function setClipboard(text) {
+
+  navigator.permissions.query({ name: 'clipboard-write' }).then(result => {
+    if (result.state === 'granted') {
+
+      let data = new DataTransfer();
+      data.items.add("text/plain", text);
+      navigator.clipboard.write(data).then(function () {
+        console.log(text);
+      }, function () {
+        console.log('copy fail', text);
+      });
+
+    } else if (result.state === 'prompt') {
+
+    } else {
+      console.log('当前页面无法获取复制权限！')
+    }
+  })
+}
+
+function copy(text) {
+  var id = 'mycustom-clipboard-textarea-hidden-id';
+  var existsTextarea = document.getElementById(id);
+
+  if (!existsTextarea) {
+    var textarea = document.createElement('textarea');
+    textarea.id = id;
+    textarea.style.position = 'fixed';
+    textarea.style.top = '-1px';
+    textarea.style.left = '-1px';
+    textarea.style.width = '1px';
+    textarea.style.height = '1px';
+    textarea.style.padding = 0;
+    textarea.style.border = 'none';
+    textarea.style.outline = 'none';
+    textarea.style.boxShadow = 'none';
+    textarea.style.background = 'transparent';
+
+    document.querySelector('body').appendChild(textarea);
+    existsTextarea = document.getElementById(id);
+  }
+
+  existsTextarea.value = text;
+  existsTextarea.select();
+
+  return new Promise((resolve, reject) => {
+    try {
+      var status = document.execCommand('copy');
+      if (status) {
+        resolve();
+      } else {
+        reject(new Error('复制失败！'));
+      }
+    } catch (err) {
+      reject(err);
+    }
+  });
+}
 
