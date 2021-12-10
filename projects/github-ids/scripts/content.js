d212114add6d14859a7c1dabc24ef0601a9b3506
dogodo-cc <741484865@qq.com>
2021-12-10 15:10:32 +0800

    feat: 增加复制提示和搜索功能

--- projects/github-ids/scripts/content.js
+++ projects/github-ids/scripts/content.js
@@ -1,16 +1,28 @@
 console.log('我是 @cocos-fe/githubs-ids 小插件，我被启用了！');
 
 const $panel = document.createElement('div');
-$panel.id = 'githubs-ids';
+$panel.id = 'github-ids';
+
+const tipText = '点击名称复制！';
+let timer = null;
+let idsMap = {}; // 名单列表
+const storageKey = 'cocos-github-ids'
+
+chrome.storage.sync.get([storageKey], (result) => {
+  console.log('storage.get:', result[storageKey]);
+  idsMap = result[storageKey];
+});
 
 function createPanel() {
   const image = chrome.runtime.getURL('images/logo.png');
-
+  const updateImage = chrome.runtime.getURL('images/update.png');
   $panel.innerHTML = `
     <div class="icon">
       <img src="${image}" alt="githubs-ids" />
     </div>
+    <input type="text" />
     <ul></ul>
+    <div class="tip"><span>${tipText}</span> <img title="更新数据" width="14" height="14" src="${updateImage}" /></div>
   `;
 
   $panel.querySelector('.icon').addEventListener('click', () => {
@@ -20,15 +32,51 @@ function createPanel() {
     }
   }, false);
 
+  $panel.querySelector('input').addEventListener('input', e => {
+    search(e.data);
+  })
+
   $panel.querySelector('ul').addEventListener('click', ({ target }) => {
-    copy(target.innerText);
+    if (target.innerText) {
+      copy(target.innerText).then(message);
+    }
+  })
+
+  $panel.querySelector('.tip').addEventListener('click', () => {
+    update();
   })
   document.body.appendChild($panel);
 }
 
-function createList(data) {
+// 手动更新数据
+async function update() {
+  await fetchList();
+  createList();
+  replaceIds();
+}
+
+function search(value) {
+  if (value) {
+    const list = Object.entries(idsMap).reduce((result, next) => {
+      const [k, v] = next;
+      if (v.includes(value)) {
+        result.push(next);
+      }
+      return result;
+    }, [])
+    createList(list);
+  } else {
+    createList();
+  }
+}
+
+// 创建列表
+function createList(list) {
+  if (!list) {
+    list = Object.entries(idsMap);
+  }
   const fragment = document.createDocumentFragment();
-  Object.entries(data).forEach(([k, v]) => {
+  list.forEach(([k, v]) => {
     const li = document.createElement('li');
     li.innerHTML = `<div class="en">${k}</div><div class="zh">${v}</div>`;
     fragment.appendChild(li);
@@ -37,38 +85,33 @@ function createList(data) {
   $panel.querySelector('ul').appendChild(fragment);
 }
 
-// 替换id
-function replaceIds() {
+// 更新数据
+function fetchList() {
   const url = `https://90s.oss-cn-hangzhou.aliyuncs.com/github-ids/github-ids.json?v=${Date.now()}`;
-
-  fetch(url).then(res => res.json()).then(data => {
-    createList(data);
-
-    Array.from(document.querySelectorAll('.author, .assignee span, .TimelineItem-body a span'))
-      .forEach(ele => {
-        const text = ele.innerText;
-        const textZH = data[text];
-        if (!ele.dataset.github) {
-          ele.dataset.github = text;
-        }
-        if (textZH) {
-          ele.innerText = textZH;
-        }
-      })
-  })
+  console.log('fetch', url);
+  return fetch(url).then(res => res.json()).then(data => {
+    idsMap = data;
+    chrome.storage.sync.set({ [storageKey]: data }, () => {
+      console.log('storage.set:', data);
+    });
+    return data;
+  });
 }
 
-// 监听路由等事件
-['hashchange', 'popstate', 'load'].forEach(e => {
-  window.addEventListener(e, () => {
-    replaceIds();
-    if (e === 'load') {
-      observerProgress();
-      createPanel();
-    }
-  })
-})
-
+// 替换页面id
+function replaceIds() {
+  Array.from(document.querySelectorAll('.author, .assignee span, .TimelineItem-body a span'))
+    .forEach(ele => {
+      const text = ele.innerText;
+      const textZH = idsMap[text];
+      if (!ele.dataset.github) {
+        ele.dataset.github = text;
+      }
+      if (textZH) {
+        ele.innerText = textZH;
+      }
+    });
+}
 
 // 通过监听 github 的页面切换进度条来判断是否完成路由切换
 function observerProgress() {
@@ -88,29 +131,7 @@ function observerProgress() {
   // observer.disconnect();
 }
 
-
-
-function setClipboard(text) {
-
-  navigator.permissions.query({ name: 'clipboard-write' }).then(result => {
-    if (result.state === 'granted') {
-
-      let data = new DataTransfer();
-      data.items.add("text/plain", text);
-      navigator.clipboard.write(data).then(function () {
-        console.log(text);
-      }, function () {
-        console.log('copy fail', text);
-      });
-
-    } else if (result.state === 'prompt') {
-
-    } else {
-      console.log('当前页面无法获取复制权限！')
-    }
-  })
-}
-
+// 复制
 function copy(text) {
   var id = 'mycustom-clipboard-textarea-hidden-id';
   var existsTextarea = document.getElementById(id);
@@ -150,3 +171,31 @@ function copy(text) {
   });
 }
 
+// 复制成功提示
+function message() {
+  if (timer === null) {
+    $panel.querySelector('.tip span').innerText = '复制成功！';
+    timer = window.setTimeout(() => {
+      timer = null;
+      $panel.querySelector('.tip span').innerText = tipText;
+    }, 1500)
+  } else {
+    window.clearTimeout(timer);
+    timer = null;
+  }
+}
+
+// 监听路由等事件
+['hashchange', 'popstate', 'load'].forEach(ev => {
+  window.addEventListener(ev, async () => {
+    replaceIds();
+    if (ev === 'load') {
+      if (Object.keys(idsMap).length === 0) {
+        await fetchList();
+      }
+      observerProgress();
+      createPanel();
+      createList();
+    }
+  })
+})
