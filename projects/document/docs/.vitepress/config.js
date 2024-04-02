5f4d1281b03a02687aa7d88bf267875676d45e1f
alan <741484865@qq.com>
2024-04-02 14:55:09 +0800

    feat(docs): update

--- projects/document/docs/.vitepress/config.js
+++ projects/document/docs/.vitepress/config.js
@@ -89,7 +89,7 @@ function getCoreSidebar() {
             items: [
                 { text: 'Eslint', link: '/core/eslint' },
                 { text: 'Eslint 工作流', link: '/core/eslint-工作流' },
-                { text: 'nvm 安装', link: '/core/nvm/nvm' },
+                { text: 'nvm 安装', link: '/core/nvm/install' },
                 { text: 'nvm 命令行执行', link: '/core/nvm/nvm-spawn' },
                 { text: '最佳实践-前端工程', link: '/core/前端项目工程实践建议' },
                 { text: '最佳实践-命名', link: '/core/intitle' },
